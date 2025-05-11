8db5963cb3c870a3cec9d2312c0e0323528c389b
DerkNiessink <derk@niessink.com>
2025-05-11 23:00:30 +0200

    Created Calendar class

--- backend/read.py
+++ backend/read.py
@@ -1,56 +1,76 @@
-from icalendar import Calendar
+from icalendar import Calendar as iCalendar
 from datetime import datetime
+from datetime import timezone
+from dateutil import tz
 
 from event import Event
 
 
-def read_ics(fn: str) -> Calendar:
-    """Reads an ICS file and returns its content."""
-    with open(fn, "rb") as f:
-        return Calendar.from_ical(f.read())
+class Calendar:
+    def __init__(self, fn: str):
+        with open(fn, "rb") as f:
+            self.components = iCalendar.from_ical(f.read())
+        self.events = self._get_events()
 
+    def _get_events(self) -> list[Event]:
+        """
+        Get all events from the calendar.
 
-def get_events(fn="ics/sacha@wearebit.com.ics") -> list[Event]:
-    cal = read_ics(fn)
-    events = []
-    for component in cal.walk():
-        if component.name == "VEVENT":
-            starttime, endtime = component.get("dtstart"), component.get("dtend")
-            if endtime:
-                events.append(Event(starttime.dt, endtime.dt))
-    return events
+        Returns:
+            list[Event]: List of events in the calendar.
+        """
+        events = []
+        for component in self.components.walk():
+            if component.name == "VEVENT":
+                starttime, endtime = component.get("dtstart"), component.get("dtend")
+                if endtime:
+                    events.append(Event(starttime.dt, endtime.dt))
+        return events
 
+    def get_free_events(self, event: Event) -> list[Event]:
+        """
+        Get free events inside the given event.
 
-def find_free_slots(
-    events: list[Event], start_day: datetime, end_day: datetime
-) -> list[Event]:
+        Args:
+            start_day (datetime): Start day to check for free events.
+            end_day (datetime): End day to check for free events.
 
-    free_events = []
-    current = start_day
-    for event in events:
+        Returns:
+            list[Event]: List of free events between `start_day` and `end_day`.
+        """
 
-        try:
-            if event.endtime.date() != start_day.date():
+        free_events = []
+        current = start_day
+        for event in self.events:
+
+            try:
+                if event.endtime.date() != start_day.date():
+                    continue
+            except AttributeError:
                 continue
-        except AttributeError:
-            continue
 
-        print(event.starttime, start_day)
-        event_start = max(event.starttime, start_day)
-        event_end = min(event.endtime, end_day)
-        if event_start > current:
-            free_events.append(Event(current, event_start))
+            event_start = max(event.starttime, start_day)
+            event_end = min(event.endtime, end_day)
+            if event_start > current:
+                free_events.append(Event(current, event_start))
 
-        current = max(current, event_end)
+            current = max(current, event_end)
 
-    if current < end_day:
-        free_events.append(Event(current, end_day))
+        if current < end_day:
+            free_events.append(Event(current, end_day))
 
-    return free_events
+        return free_events
 
 
 if __name__ == "__main__":
-    events = get_events()
-    start_day = datetime(2025, 5, 7)
-    end_day = datetime(2025, 5, 7)
-    free_slots = find_free_slots(events, start_day, end_day)
+    calendar = Calendar("ics/sacha@wearebit.com.ics")
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
+
+    start_day = datetime(2025, 5, 7).replace(tzinfo=from_zone)
+    start_day = start_day.astimezone(to_zone)
+    end_day = datetime(2025, 5, 7, 23, 59).replace(tzinfo=from_zone)
+    end_day = end_day.astimezone(to_zone)
+
+    free_slots = calendar.get_free_events(start_day, end_day)
+    print(free_slots)
