a28126100a470a92b73cb875610f71c5091b9742
sachab01 <sacha.buijs@student.uva.nl>
2025-05-12 22:13:35 +0200

    find all overlaps

--- backend/planner.py
+++ backend/planner.py
@@ -1,18 +1,59 @@
 from datetime import datetime
+import numpy as np
 from event import Event
 
 
-def find_overlaps(calendars: list[list[Event]]) -> dict[tuple[int, int], Event]:
+def find_pairwise_overlaps(
+    calendars: list[list[Event]],
+) -> (
+    dict[tuple[int, int], list[Event]]
+    | dict[tuple[tuple[int, ...], tuple[int, ...]], list[Event]]
+):
     """
-    Check if calendars overlap.
+    Find overlapping events between each pair of calendars.
+    Returns a dictionary mapping calendar index pairs to lists of their overlapping events.
     """
     overlaps = {}
     for i in range(len(calendars)):
         for j in range(i + 1, len(calendars)):
-            if i == j:
-                continue
             for event1 in calendars[i]:
                 for event2 in calendars[j]:
                     if event1.overlaps(event2):
-                        overlaps[(i, j)] = event1.return_overlap(event2)
+                        overlap = event1.return_overlap(event2)
+                        overlaps.setdefault((i, j), []).append(overlap)
     return overlaps
+
+
+def merge_overlaps(
+    overlaps: dict[tuple[int, ...], list[Event]],
+) -> dict[tuple[int, ...], list[Event]]:
+    """
+    Merge overlapping events across multiple overlapping pairs to find common free slots.
+    """
+    # create a list of lists from all values in the dictionary
+    overlaps_list = list(overlaps.values())
+    new_overlaps = find_pairwise_overlaps(overlaps_list)
+    merged_overlaps = {}
+
+    for (key1, key2), value in new_overlaps.items():
+        keys_list = list(overlaps.keys())
+        new_key = tuple(set(keys_list[key1] + keys_list[key2]))
+        merged_overlaps[new_key] = value
+
+    return merged_overlaps
+
+
+def find_all_overlaps(
+    calendars: list[list[Event]],
+) -> list[dict[tuple[int, ...], list[Event]]]:
+    """
+    Iteratively find all common overlapping time slots across all calendars.
+    Returns a list of overlap dictionaries at each level of merging.
+    """
+    overlaps = [find_pairwise_overlaps(calendars)]
+
+    # while not empty dict
+    while overlaps[-1]:
+        overlaps.append(merge_overlaps(overlaps[-1]))
+
+    return overlaps[:-1]
